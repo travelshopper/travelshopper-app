04cccb138c34bbb8a3b2834108da76a3d47c2cc3
startup6ky <startup6.ky@gmail.com>
2015-10-31 00:42:32 +0000

    add wish item images to wish item form

--- app/models/wish_item.rb
+++ app/models/wish_item.rb
@@ -3,4 +3,6 @@ class WishItem < ActiveRecord::Base
 
   belongs_to :itinerary_item
   has_many :wish_item_images, dependent: :destroy
+
+  accepts_nested_attributes_for :wish_item_images, allow_destroy: true
 end
